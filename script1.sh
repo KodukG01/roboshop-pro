d58f1f97b7aaf8ba5c8516d22d4a631c95656c9b
GautamKumar <kodukulagautamkumar@gmail.com>
2025-05-22 19:49:34 +0530

    new file added

--- script1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-Greetings="Hello, Good morning"
-echo "$Greetings"
-echo "PID of script1: $$"
-
-./script1.sh
-
-
--- script2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "PID of script2: $$"
-echo "$Greetings"
-
-
